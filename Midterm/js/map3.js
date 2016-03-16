593175ecf4e5e4e22ffcc1808005ea045253d53f
mreave3 <matthewtreaves@gmail.com>
2016-03-16 13:40:03 -0500

    Added files via upload

---
+++ Midterm/js/map3.js
@@ -0,0 +1,22 @@
+ var map;
+	require(["esri/map", "esri/dijit/BasemapToggle", "esri/layers/ArcGISDynamicMapServiceLayer", "dojo/domReady!"], 
+	function(Map, BasemapToggle, ArcGISDynamicMapServiceLayer) {
+        map = new Map("map", {
+          basemap: "topo",  //For full list of pre-defined basemaps, navigate to http://arcg.is/1JVo6Wd
+          center: [-90, 30], // longitude, latitude
+          zoom: 4
+        });
+		
+		// Create a layer object from an ArcGIS Server web service, with no options
+var layer1 = new ArcGISDynamicMapServiceLayer( "http://nowcoast.noaa.gov/arcgis/rest/services/nowcoast/mapoverlays_political/MapServer" );
+			map.addLayer(layer1); // add the layer object to the map
+		var toggle = new BasemapToggle({
+        map: map,
+        basemap: "satellite"
+      }, "BasemapToggle");
+      toggle.startup();
+      
+});
+	  
+
+
